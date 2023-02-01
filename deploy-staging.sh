2b0670c3ee334ec48901007f8bdbc78514e0815b
Jiho Lee <optional.int@kakao.com>
2023-02-01 04:18:23 +0000

    fix: do not delete staging env

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,10 +2,10 @@
 
 kubectl apply -f staging.namespace.yaml
 
-kubectl delete -k auth/overlay/staging --wait
-kubectl delete -k images/overlay/staging --wait
-kubectl delete -k backend/overlay/staging --wait
-kubectl delete -k frontend/overlay/staging --wait
+# kubectl delete -k auth/overlay/staging --wait
+# kubectl delete -k images/overlay/staging --wait
+# kubectl delete -k backend/overlay/staging --wait
+# kubectl delete -k frontend/overlay/staging --wait
 
 kubectl apply -k auth/overlay/staging
 kubectl apply -k images/overlay/staging
